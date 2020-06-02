0f09e117773dcb52230556b52c998462600d52c8
Alice Russell <alice_r1295@hotmail.com>
2020-06-01 23:19:49 -0400

    Initial implementation of live evaluation of multiple expressions.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,40 @@
 const evaluateAndOutput = (interactions, evalElement) => {
-    evalElement.value = eval(interactions);
+    let collections = [];
+    let expressions = [];
+    let buffer = "";
+    for (let elem of interactions) {
+        if (elem == "\n") {
+            if (! buffer) {
+                expressions.push(collections.reduce((a, b) => a.concat(b)));
+                collections = [];
+            } else {
+                collections.push(buffer);
+            }
+            buffer = "";
+        } else {
+            buffer += elem;
+        }
+    }
+
+    if (buffer) {
+        collections.push(buffer);
+    }
+    if (collections) {
+        expressions.push(collections.reduce((a, b) => a.concat(b)));
+    }
+
+    console.log(expressions);
+    for (let elem of expressions) {
+        console.log(elem);
+    }
+
+    evalElement.value = "";
+
+    for (let elem of expressions) {
+        if (String(evalElement.value)) {
+            evalElement.value = String(evalElement.value) + "\n" + String(eval(elem));
+        } else {
+            evalElement.value = String(eval(elem));
+        }
+    }
 }
