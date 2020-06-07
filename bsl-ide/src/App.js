d3c0ba5bf51cca69d9c178b71316a09e67925955
Alice Russell <alice_r1295@hotmail.com>
2020-06-07 18:53:42 -0400

    text copying ported to react. Doesn't evaluate yet though.

--- bsl-ide/src/App.js
+++ bsl-ide/src/App.js
@@ -3,28 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 import evaluateAndOutput from './script';
 
-function App() {
-  return (
-    <div class="row">
-      <Interactions />
-      <Evaluations />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(e) {
+    this.setState({code: e.target.value});
+  }
+
+  render() {
+    return (
+      <div class="row">
+        <Interactions
+          code = {this.state.code}
+          handleChange = {this.handleChange}
+        />
+        <Evaluations code={this.state.code}/>
+      </div>
+    );
+  }
 }
 
-function Interactions(props) {
-  return (
-    <div class="column">
-      <textarea
-        id="interactions"
-        name="interactions"
-        rows="40"
-        cols="80"
-        placeholder="Interactions"
-        onInput={evaluateAndOutput}
-      ></textarea>
-  </div>
-  );
+class Interactions extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="column">
+        <textarea
+          id="interactions"
+          name="interactions"
+          rows="40"
+          cols="80"
+          placeholder="Interactions"
+          value={this.props.code}
+          onChange={this.props.handleChange} 
+        />
+      </div>
+    );
+  }
 }
 
 function Evaluations(props) {
@@ -37,6 +62,7 @@ function Evaluations(props) {
         cols="80"
         placeholder="Evaluations"
         readonly="true"
+        value={props.code}
       ></textarea>
     </div>
   );
