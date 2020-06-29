8aeb001b98cfa5e2332660a13689c64692749bad
Alice Russell <alice_r1295@hotmail.com>
2020-06-29 11:09:29 -0400

    oops didnt push tests

--- test/parse.spec.js
+++ test/parse.spec.js
@@ -1,4 +1,4 @@
-const { tokenize, parse } = require('../logic/parse.js');
+const { tokenize, parse, parseSexp, parseSexps} = require('../logic/parse.js');
 const { expect } = require('chai');
 
 function Tok(t, v) { return { type: t, value: v} ; }
@@ -16,6 +16,9 @@ const [ CP, OP, SPACE, OSP, CSP, OBP, CBP ] =
 function Num(v) { return Tok('Number', v.toString()); }
 function Id(v) { return Tok('Identifier', v); }
 function String(v) { return Tok('String', '"' + v + '"'); }
+function ResultSuccess(t, r) { return {thing: t, remain: r} }
+function ResultFailure(e, r) { return {error: e, remain: r} }
+
 
 const checkExpect = (res, expected) => {
     expect(res).to.deep.equal(expected);
@@ -38,12 +41,12 @@ describe('checkExpectMultiple', () => {
 })
 
 describe('tokenize', () => {
-    it('should return the empty list for an empty string', () => {  
+    it('should handle the base case correctly', () => {  
         const result = tokenize('');
         checkExpect(result, []);
     });
 
-    it('should tokenize a bunch of parenthesis correctly', () => {
+    it('should tokenize parenthesis correctly', () => {
         const result = tokenize('([[[][][][][][])))[][])))){}{}{}');
         const expected = [
             OP, OSP, OSP, OSP, CSP, OSP, CSP, OSP, CSP, OSP, CSP, OSP,
@@ -66,85 +69,93 @@ describe('tokenize', () => {
         checkExpect(result, expected);
     });
 
-    it('should tokenize booleans correctly', () => {
-        function result() { tokenize('#t123'); }
-        expect(tokenize("#t")).to.deep.equal([Tok('Boolean', '#t')]);
-        expect(tokenize("#f")).to.deep.equal([Tok('Boolean', '#f')]);
-        expect(tokenize("#true")).to.deep.equal([Tok('Boolean', '#true')]);
-        expect(tokenize("#false")).to.deep.equal([Tok('Boolean', '#false')]);
-        expect(result).to.throw(Error);
-    });
+    // it('should tokenize booleans correctly', () => {
+    //     const result = ['#t', '#f', '#true', '#false'];
+    //     const expected = [
+    //         [Tok('Boolean', '#t')],
+    //         [Tok('Boolean', '#f')],
+    //         [Tok('Boolean', '#true')],
+    //         [Tok('Boolean', '#false')]
+    //     ];
+    //     checkExpectMultiple(
+    //         tokenize, invalidBoolean, expected
+    //     );
+    //     function invalidBoolean() { tokenize('#t123'); }
+    //     expect(invalidBoolean).to.throw(Error);
+    // });
 
-    it('should tokenize factorial correctly', () => {
-        let result = tokenize('(define (fact n) (if (= n 0) 1 (* n (fact (- n 1)))))')
-        let expected = [
-            OP,
-            Id('define'),
-            SPACE,
-            OP,
-            Id('fact'),
-            SPACE,
-            Id('n'),
-            CP,
-            SPACE,
-            OP,
-            Id('if'),
-            SPACE,
-            OP,
-            Id('='),
-            SPACE,
-            Id('n'),
-            SPACE,
-            Num(0),
-            CP,
-            SPACE,
-            Num(1),
-            SPACE,
-            OP,
-            Id('*'),
-            SPACE,
-            Id('n'),
-            SPACE,
-            OP,
-            Id('fact'),
-            SPACE,
-            OP,
-            Id('-'),
-            SPACE,
-            Id('n'),
-            SPACE,
-            Num(1),
-            CP,
-            CP,
-            CP,
-            CP,
-            CP,
-        ];
-        checkExpect(result, expected);
-    });
+    // it('should tokenize factorial correctly', () => {
+    //     let result = tokenize('(define (fact n) (if (= n 0) 1 (* n (fact (- n 1)))))')
+    //     let expected = [
+    //         OP,
+    //         Id('define'),
+    //         SPACE,
+    //         OP,
+    //         Id('fact'),
+    //         SPACE,
+    //         Id('n'),
+    //         CP,
+    //         SPACE,
+    //         OP,
+    //         Id('if'),
+    //         SPACE,
+    //         OP,
+    //         Id('='),
+    //         SPACE,
+    //         Id('n'),
+    //         SPACE,
+    //         Num(0),
+    //         CP,
+    //         SPACE,
+    //         Num(1),
+    //         SPACE,
+    //         OP,
+    //         Id('*'),
+    //         SPACE,
+    //         Id('n'),
+    //         SPACE,
+    //         OP,
+    //         Id('fact'),
+    //         SPACE,
+    //         OP,
+    //         Id('-'),
+    //         SPACE,
+    //         Id('n'),
+    //         SPACE,
+    //         Num(1),
+    //         CP,
+    //         CP,
+    //         CP,
+    //         CP,
+    //         CP,
+    //     ];
+    //     checkExpect(result, expected);
+    // });
 
-    it('should tokenize strings correctly', () => {
-        const result = [
-            '"abc" def "ghi"',
-            '"abc"def"ghi"'
-        ];
-        const expected = [
-            [String('abc'), SPACE, Id('def'), SPACE, String('ghi')],
-            [String('abc'), Id('def'), String('ghi')]
-        ];
+    // it('should tokenize strings correctly', () => {
+    //     const result = [
+    //         '"abc" def "ghi"',
+    //         '"abc"def"ghi"'
+    //     ];
+    //     const expected = [
+    //         [String('abc'), SPACE, Id('def'), SPACE, String('ghi')],
+    //         [String('abc'), Id('def'), String('ghi')]
+    //     ];
 
-        checkExpectMultiple(tokenize, result, expected);
-    });
+    //     checkExpectMultiple(tokenize, result, expected);
+    // });
 });
 
 describe('parseSexp', () => {
-    it('fails on these cases', () => {
+    it('', () => {
         const result = [
             ') (hello)',
             '(define (fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))',
+            'define (fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))',
+            '(fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))'
         ];
         const expected = [
-            ResultFailure(TooManyParens, ') (hello)'),
+            ResultFailure('Found an unmatched closing parenthesis', tokenize(') (hello)')),
             ResultSuccess(
                 [
                     Id('define'),
@@ -175,6 +186,17 @@ describe('parseSexp', () => {
                     ]
                 ],
                 []
+            ),
+            ResultSuccess(
+                Id('define'),
+                tokenize('(fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))').filter(x => x.type !== SPACE)
+            ),
+            ResultSuccess(
+                [
+                    Id('fact'),
+                    Id('n')
+                ],
+                tokenize('(if (= n 0) 1 (* n (fact (- 1 n)))))')
             )
         ];
         checkExpectMultiple(parseSexp, result, expected);
@@ -182,61 +204,146 @@ describe('parseSexp', () => {
 });
 
 describe('parseSexps', () => {
-
-});
-
-describe('parse', () => {
-    it('should parse this fact function', () => {
-        const result = parse('(define (fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))');
-        const expected = (
-            [
-                Id('define'),
-                [ 
-                    Id('fact'),
-                    Id('n'),
-                ],
+    it('', () => {
+        const result = [
+            'define (fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))',
+            '(fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))'
+        ];
+        const expected = [
+            ResultSuccess(
                 [
-                    Id('if'),
-                    [ 
-                        Id('='),
-                        Id('n'),
-                        Num(0),
-                    ],
-                    Num(1),
+                    Id('define'),
                     [
-                        Id('*'),
-                        Id('n'),
+                        Id('fact'),
+                        Id('n')
+                    ],
+                    [
+                        Id('if'),
                         [
-                            Id('fact'),
+                            Id('='),
+                            Id('n'),
+                            Num('0'),
+                        ],
+                        Num(1),
+                        [
+                            Id('*'),
+                            Id('n'),
                             [
-                                Id('-'),
-                                Id('n'),
-                                Num(1)
+                                Id('fact'),
+                                [
+                                    Id('-'),
+                                    Num('1'),
+                                    Id('n')
+                                ]
                             ]
                         ]
-                    ] 
-                ]
-            ]
-        );
-        checkExpect(result, expected);
+                    ]
+                ],
+                [CP]
+            ),
+            ResultSuccess(
+                [
+                    [
+                        Id('fact'),
+                        Id('n')
+                    ],
+                    [
+                        Id('if'),
+                        [
+                            Id('='),
+                            Id('n'),
+                            Num('0'),
+                        ],
+                        Num(1),
+                        [
+                            Id('*'),
+                            Id('n'),
+                            [
+                                Id('fact'),
+                                [
+                                    Id('-'),
+                                    Num('1'),
+                                    Id('n')
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                [CP]
+            )
+        ];
+        checkExpectMultiple(parseSexps, result, expected);
     });
 });
 
-// tokenizer should never lose characters
-// parser has to deal with whitespace tokens
+describe('parse', () => {
+    it('', () => {
+        const result = [
+            '',
+            // '(',
+            // '[',
+            // '{',
+            ')',
+            ']',
+            '}',
+            '123',
+            '"hello"',
+            'x',
+            '#true',
+            // '(define x 10)'
+        ];
+        const expected = [
+            tokenize(''),
+            // tokenize('('),
+            // tokenize('['),
+            // tokenize('{'),
+            tokenize(')'),
+            tokenize(']'),
+            tokenize('}'),
+            tokenize('123'),
+            tokenize('"hello"'),
+            tokenize('x'),
+            tokenize('#true'),
+            // [Id('define'), Id('x'), Num('10')]
+        ];
+        checkExpectMultiple(parse, result, expected);
+    });
 
-
-// parseSexp: [Tokens] -> Result??
-
-// // A Result is one of:
-// // - {sexp: SExp, remain: [Token]}
-// // - {error: String, remain: [Token]}
-
-// // An SExp is one of:
-// // - String
-// // - Number
-// // -
-// // - [SExp]
+    // it('should parse this fact function', () => {
+    //     const result = parse('(define (fact n) (if (= n 0) 1 (* n (fact (- 1 n)))))');
+    //     const expected = (
+    //         [
+    //             Id('define'),
+    //             [ 
+    //                 Id('fact'),
+    //                 Id('n'),
+    //             ],
+    //             [
+    //                 Id('if'),
+    //                 [ 
+    //                     Id('='),
+    //                     Id('n'),
+    //                     Num(0),
+    //                 ],
+    //                 Num(1),
+    //                 [
+    //                     Id('*'),
+    //                     Id('n'),
+    //                     [
+    //                         Id('fact'),
+    //                         [
+    //                             Id('-'),
+    //                             Id('n'),
+    //                             Num(1)
+    //                         ]
+    //                     ]
+    //                 ] 
+    //             ]
+    //         ]
+    //     );
+    //     checkExpect(result, expected);
+    // });
+});
 
 // parseSExp(tokenize(“(  define x 10) blah )”))
 // parseSExp(tokenize(“(  define x 10\n) blah )”))
