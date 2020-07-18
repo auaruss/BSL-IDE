0f39c3ab8f4c99c7733de5c9b11f5ed9f6a6869a
Alice Russell <alice_r1295@hotmail.com>
2020-07-18 14:38:32 -0400

    Create predicate for TokenError

--- src/logic/predicates.ts
+++ src/logic/predicates.ts
@@ -2,6 +2,12 @@ import {
   DefOrExpr, Definition, Expr, Result, TokenError
 } from './types';
 
+export const isTokenError = (x: any): x is TokenError => {
+  if (! (typeof x === 'object')) return false;
+  if (! (x.error && typeof x.error === 'string')) return false;
+  if (! (x.value && typeof x.value === 'string')) return false;
+  return x.error === 'Unidentified Token';
+}
 
 // Checks to see if a specific DefOrExpr is an Expr.
 export const defOrExprIsExpr = (d: DefOrExpr): d is Expr => {
